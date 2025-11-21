e3356e50ff965a970a80cbed76dd550701ff6000
hamzaamir-design <hamzaamir9733@gmail.com>
2025-11-21 14:32:51 +0500

    Completed Hero Section

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../../styles/styles";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div
+      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.normalFlex}`}
+      style={{
+        backgroundImage:
+          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+        <h1 className="text-[35px] leading-[1.2] 800px:text-60px text-[#3d3a3a] font-600px capitalize">
+          Best Collection for <br /> Home Decoration
+        </h1>
+        <p className="pt-5 text-[16px] font-[Poppins] font-400px text-[#000000ba] leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
+          <br />
+          assumenda? Quisquam itaque exercitationem labore vel, dolore quidem
+          <br />
+          asperiores, laudantium temporibus soluta optio consequatur aliquam
+          <br />
+          deserunt officia. Dolorum saepe nulla provident.
+        </p>
+        <Link to="/products" className="inline-block">
+          <div className={`${styles.button} mt-5`}>
+            <span className="text-white font-[Poppins] text-[18px]">
+              Shop Now
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
