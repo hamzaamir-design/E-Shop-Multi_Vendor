272fe5281a4ab855f34ab1009afa1e67d8b27b7b
hamzaamir-design <hamzaamir9733@gmail.com>
2025-11-26 14:55:41 +0500

    Completed the sponsored component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
 import Events from "../components/Events/Events";
-// import Sponsored from "../components/Route/Sponsored";
+import Sponsored from "../components/Route/Sponsored";
 // import Footer from "../components/Layout/Footer";
 
 const HomePage = () => {
@@ -18,8 +18,8 @@ const HomePage = () => {
       <BestDeals />
       <Events />
       <FeaturedProduct />
-      {/* <Sponsored />
-        <Footer /> */}
+      <Sponsored />
+        {/* <Footer />  */}
     </div>
   )
 }
